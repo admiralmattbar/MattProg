d3ec2c78875233e19d419c4ea2ef9c2e5764a1b7
admiralmattbar <mdkissel@gmail.com>
2017-09-22 00:34:28 -0700

    working with awt

--- src/gui/GuiWindow.java
+++ src/gui/GuiWindow.java
@@ -3,42 +3,61 @@ package gui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 
 public class GuiWindow extends Frame implements ActionListener
-{	
-	
+{
+
 	private static final long serialVersionUID = 1L;
 	private TextField tf;
 	private int i = 0;
-	
+	public static ArrayList<String> students;
+
+
+	int clickCount = 0;
+
 	public GuiWindow()
 	{
-		
+
 		//Setting up the frame
 		setSize(600, 400);
 		setLayout(null);
 		setVisible(true);
-		
+
 		//Creating the only button
 		Button but = new Button("Please Click Me!");
-		but.setBounds(10,30,180,30);
-		but.addActionListener(this); //add action listener to button
+		but.setBounds(300,380,180,30);
+		//but.addActionListener(this); //add action listener to button
 		add(but);
-		
+
 		//Creating a text field
-		tf = new TextField(Integer.toString(i));
+		Font nameFont = new Font("SansSerif", Font.PLAIN, 24);
+		tf = new TextField();
+		tf.addActionListener(this);
 		tf.setBounds(250, 220, 250, 40);
+		tf.setFont(nameFont);
 		add(tf);
-		
+
+
+		//Close window when exit button is clicked
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we){
+				dispose();
+			}
+		});
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		i++;
-		tf.setText(Integer.toString(i));
-		
+
+		students.add(tf.getText());
+		System.out.print(students);
+		tf.setText("");
+
 	}
 
 }
