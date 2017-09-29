6f5c97affe3d4d1f5574150d9c9aa119107a16ee
teacher <teacher@Spinel.local>
2017-09-29 14:18:32 -0700

    added random generation

--- src/MattProg.java
+++ src/MattProg.java
@@ -1,14 +1,33 @@
+import java.awt.event.KeyEvent;
+
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+
 import gui.GuiWindow;
 
 
 public class MattProg 
 {
-	public GuiWindow gw;
+	//public static GuiWindow gw;
 	
 	public static void main(String[] args)
 	{
 		
 		new GuiWindow();
+		
+		JMenuBar menuBar;
+		JMenu menu, submenu;
+		JMenuItem menuItem;
+		JRadioButtonMenuItem rbMenuItem;
+		JCheckBoxMenuItem cbMenuItem;
+		
+		menuBar = new JMenuBar();
+		menu = new JMenu("File");
+		
+		menuBar.add(menu);
 		
 	}
 	
--- src/gui/GuiWindow.java
+++ src/gui/GuiWindow.java
@@ -5,44 +5,121 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Random;
 
 
-public class GuiWindow extends Frame implements ActionListener
+public class GuiWindow extends Frame
 {
 
 	private static final long serialVersionUID = 1L;
 	private TextField tf;
-	//public static ArrayList<String> students;
-	public static String[] students;
+	public static String[] students; //Array that holds the student names
 	private int i = 0;
 	private int text_y = 30;
 	private int text_x = 10;
 	private TextField sn;
+	private TextField randName;
+	private ActionListener addName;
+	private ActionListener getName;
+	private Font nameFont = new Font("SansSerif", Font.PLAIN, 24);
 
 	
 
 	public GuiWindow()
 	{
 		students = new String[24];
+		
+		//Action listener to add names
+		addName = new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				String name = tf.getText();
+				if(!name.isEmpty() && i < 24)
+				{	
+					students[i] = name;
+					tf.setText("");
+					i++;
+					
+					sn = new TextField(name);
+					sn.setBounds(text_x, text_y, 250, 40);
+					sn.setFont(nameFont);
+					sn.setEditable(false);
+					add(sn);
+					
+					text_y += 50;
+					
+					if(i % 8 == 0)
+					{
+						text_x += 260;
+						text_y = 30;
+					}
+				}
+			}
+		};
+		
+		//Action listener to get a pseudorandom name from the list of names.
+		getName = new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				Random rand = new Random();
+				int k = 0;
+				
+				/*
+				 * This loop makes it so that only positions in the array
+				 * that have values can be called by the random number.
+				 * k is the variable that stores the amount of filled positions
+				 * that is the basis for getting the random integer.
+				 */
+				for(int j = 0; j < students.length; j++) {
+					if(students[j] != null)
+					{
+						k++;
+					}
+				}
+				int rando = rand.nextInt(k);
+				System.out.println(students[rando]);
+				randName.setText(students[rando]);
+				
+			}
+		};
+		
 
 		//Setting up the frame
 		setSize(800, 600);
 		setLayout(null);
 		setVisible(true);
 
-		//Creating the only button
+		//Creating the add button
 		Button but = new Button("Add Student");
-		but.setBounds(getWidth()/2,500,180,30);
-		but.addActionListener(this); //add action listener to button
+		but.setBounds(getWidth()/2,500,180,40);
+		but.addActionListener(addName); //add action listener to button
+		but.setFont(nameFont);
 		add(but);
+		
+		//Creating the random button
+		Button sbut = new Button("Get Student");
+		sbut.setBounds(getWidth()/2 + 190, 500, 180, 40);
+		sbut.addActionListener(getName);
+		sbut.setFont(nameFont);
+		add(sbut);
 
-		//Creating a text field
-		Font nameFont = new Font("SansSerif", Font.PLAIN, 24);
+		//Creating a text field to enter names
 		tf = new TextField();
-		tf.addActionListener(this);
+		tf.addActionListener(addName);
 		tf.setBounds(getWidth()/2 - 260, 500, 250, 40);
 		tf.setFont(nameFont);
 		add(tf);
+		
+		//Create a text field that shows the random name
+		randName = new TextField();
+		randName.setFont(nameFont);
+		randName.setBounds(getWidth()/2 - 260, 450, 250, 40);
+		randName.setEditable(false);
+		add(randName);
 
 
 		//Close window when exit button is clicked
@@ -53,29 +130,4 @@ public class GuiWindow extends Frame implements ActionListener
 		});
 
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-
-		String name = tf.getText();
-		students[i] = name;
-		tf.setText("");
-		i++;
-		
-		Font nameFont = new Font("SansSerif", Font.PLAIN, 24);
-		sn = new TextField(name);
-		sn.setBounds(text_x, text_y, 250, 40);
-		sn.setFont(nameFont);
-		sn.setEditable(false);
-		add(sn);
-		
-		text_y += 50;
-		
-		if(i % 8 == 0)
-		{
-			text_x += 260;
-			text_y = 30;
-		}
-	}
-
 }
